9f608c57794b978ccd19f06c15fa228c384f50ba
manuel1280 <komodocr7@gmail.com>
2021-03-18 01:39:46 -0500

    basic api post created

--- application.js
+++ application.js
@@ -3,11 +3,31 @@ function getUsers() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200 ) {
             let listUsers = document.getElementById("list-users");
-            let responses = JSON.parse(this.responseText)
-            console.log(responses)
+            let responses = JSON.parse(this.responseText);
+
+            for (var i = 0; i < responses.length; i++) {
+                listUsers.insertAdjacentHTML("afterbegin",
+                    responses[i].id + " - " + responses[i].full_name + "<br />")
+            }
+
         }
     };
     xhttp.open("GET","http://localhost:3001/api/v1/users", true);
     xhttp.send();
 }
+
+window.sendUser = function(event) {
+    event.preventDefault();
+    var xhttp = new this.XMLHttpRequest();
+    xhttp.open("POST","http://localhost:3001/api/v1/users", true);
+    xhttp.onload = function (event) {
+        let listUsers = document.getElementById("list-users");
+        let response = JSON.parse(this.responseText);
+
+        listUsers.insertAdjacentHTML("afterbegin",
+            response.id + " - " + response.full_name + "<br />")
+    };
+    var userForm = new this.FormData(document.getElementById("user_form"))
+    xhttp.send(userForm)
+};
 getUsers();
